e5eaf28319510ec92e4d6b88e7cbb26079213b60
dorukgezici <dorukgezici@gmail.com>
2017-05-12 16:34:34 +0300

    Initial Commit

---
+++ OOP-HW3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OOP-HW3
+//
+//  Created by Doruk Gezici on 12/05/2017.
+//  Copyright © 2017 Doruk Gezici. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
